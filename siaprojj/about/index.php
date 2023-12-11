747b3a081bd42961768f10f65ce347670d1ed120
Shannon2121441 <99891185+Shannon2121441@users.noreply.github.com>
2023-12-12 02:46:44 +0800

    Add files via upload

---
+++ siaprojj/about/index.php
@@ -0,0 +1,5 @@
+<div id="about">
+    <img src="images/grp.jpg" alt="Tomorrow x Together" class="med-image">
+    <p>TOMORROW X TOGETHER, read as ‘tomorrow by together’, is a bright and youthful boyband aiming to become the next global breakthrough artist. 
+The band is composed of five members, SOOBIN, YEONJUN, BEOMGYU, TAEHYUN, HUENINGKAI, where each member is different from one another, shining in their own way to create synergy.</p>
+    </div>
--- siaprojj/index.php
+++ siaprojj/index.php
@@ -9,6 +9,8 @@ $id = (isset($_GET['id']) && $_GET['id'] != '') ? $_GET['id'] : '';
 <head>
     <title>TRIQUETRA</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -18,23 +20,34 @@ $id = (isset($_GET['id']) && $_GET['id'] != '') ? $_GET['id'] : '';
 <body>
 <div id="container">
     <div id="header">
-      TRIQUETRA
+    TOMORROW x TOGETHER
     </div>
     <div id="wrapper">
             <div id="menu">
                 <a href="index.php">Home</a> | 
-                <a href="index.php?page=search">Search</a> | 
-                <a href="logout.php" class="move-right">Log In<b> |</b></a>
+                <a href="index.php?page=spotify">Spotify</a> | 
+                <a href="index.php?page=giphy">Gif</a> | 
+                <a href="index.php?page=news">News</a>
             </div>
     </div>
     <div id="content-wrapper">
-            <div id="spotify">
-                Spotify API here
-            </div>
-            <div id="content">
-                Instagram API here
-            </div>
+    <?php
+        switch($page){
+            case 'spotify':
+                require_once 'spotify/index.php';
+            break;
+            case 'giphy':
+                require_once 'giphy/index.php';
+            break;
+            case 'news':
+                require_once 'news/index.php';
+            break;
+            default:
+                require_once 'about/index.php';
+            break;
+        }
+    ?>
     </div>
-</div>
+    
 </body>
 </html>
