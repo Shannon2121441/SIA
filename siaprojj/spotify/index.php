e064c9ecd612fa7f56c375e595a026ba613d28ff
Shannon2121441 <99891185+Shannon2121441@users.noreply.github.com>
2023-12-12 12:02:02 +0800

    Delete siaprojj directory

--- siaprojj/spotify/index.php
+++
@@ -1,3 +0,0 @@
-<div id="spotify">
-    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/5Vg9VMhC1a1uBAlLK3yVeT?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-</div>
