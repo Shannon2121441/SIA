fdf02f5d4db27c56d8ffdac07f4ea12885ebf00a
Shannon2121441 <99891185+Shannon2121441@users.noreply.github.com>
2023-12-12 12:00:11 +0800

    Add files via upload

---
+++ about/index.php
@@ -0,0 +1,4 @@
+<div id="about">
+    <img src="images/grp.jpg" alt="Tomorrow x Together" class="med-image">
+    <p>K-pop, short for Korean pop music, is a genre originating from South Korea known for its catchy tunes, impressive choreography, and visually appealing music videos. It features a diverse range of musical styles, blending pop, hip-hop, R&B, and electronic dance music. The global popularity of K-pop has given rise to dedicated fan communities and played a key role in promoting Korean culture worldwide.</p>
+    </div>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+$page = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : '';
+$subpage = (isset($_GET['subpage']) && $_GET['subpage'] != '') ? $_GET['subpage'] : '';
+$action = (isset($_GET['action']) && $_GET['action'] != '') ? $_GET['action'] : '';
+$id = (isset($_GET['id']) && $_GET['id'] != '') ? $_GET['id'] : '';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TRIQUETRA</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300&family=Geologica&family=Quicksand:wght@500&family=Rubik&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/custom.css?<?php echo time();?>">
+</head>
+<body>
+<div id="container">
+    <div id="header">
+    K-POP
+    </div>
+    <div id="wrapper">
+            <div id="menu">
+                <a href="index.php">Home</a> | 
+                <a href="index.php?page=spotify">Spotify</a> | 
+                <a href="index.php?page=giphy">GIFs</a> | 
+                <a href="index.php?page=news">News</a>
+            </div>
+    </div>
+    <div id="content-wrapper">
+    <?php
+        switch($page){
+            case 'spotify':
+                require_once 'spotify/index.php';
+            break;
+            case 'giphy':
+                require_once 'giphy/index.php';
+            break;
+            case 'news':
+                require_once 'news/index.php';
+            break;
+            default:
+                require_once 'about/index.php';
+            break;
+        }
+    ?>
+    </div>
+    
+</body>
+</html>
